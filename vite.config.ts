2ccc223e7c43e1c53fe9fd6afbbb86b9fc2c7290
unknown <mbockryu@gmail.com>
2025-06-06 13:14:17 +0100

    bellla

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,21 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    root: __dirname,
-    base: "/",
-    server: {
-      host: "::",
-      port: 8080
-    },
-    plugins: [
-      react(),
-      componentTagger()
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src")
-      }
-    },
-    define: {
-      __DEFINES__: JSON.stringify(env)
+export default defineConfig({
+  root: __dirname,
+  base: "/",
+  server: {
+    host: "::",
+    port: 8080
+  },
+  plugins: [
+    react()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
   }
 });
