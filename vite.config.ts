ed8e826584f7d54f72228c4143aaa9199bf849e3
unknown <mbockryu@gmail.com>
2025-06-06 12:04:08 +0100

    fix index

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: __dirname,
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080
-  },
-  plugins: [
-    react(),
-    componentTagger()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    root: __dirname,
+    base: "/",
+    server: {
+      host: "::",
+      port: 8080
+    },
+    plugins: [
+      react(),
+      componentTagger()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src")
+      }
+    },
+    define: {
+      __DEFINES__: JSON.stringify(env)
     }
   }
 });
