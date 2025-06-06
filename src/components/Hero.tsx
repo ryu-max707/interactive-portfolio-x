68308d51b1cd4baf394583c1774e12a8bdf13b22
unknown <mbockryu@gmail.com>
2025-06-06 10:38:26 +0100

    wouker

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,7 +41,7 @@ import { Github, ExternalLink } from "lucide-react";
           
 
           <p className="text-xl md:text-2xl mb-8 text-slate-300 leading-relaxed">
-             Développeur Full Stack passionné, J'aide les  start ups et entreprises à concevoir des applications web puissantes, performantes et élégantes. Spécialisé Laravel + React js , livewire, je construis des solutions sur-mesure du back-end à l'interface utilisateur.
+             Développeur Full Stack passionné, J'aide les  start-ups et entreprises à concevoir des applications web puissantes, performantes et élégantes. Spécialisé Laravel + React js , livewire, je construis des solutions sur-mesure du back-end à l'interface utilisateur.
 
 🔍 Disponible  pour missions freelance, projets tech et collaborations sérieuses.
           </p>
