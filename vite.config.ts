609387cf4e8c1b1d2b00e6490d3d9b460ebd109f
unknown <mbockryu@gmail.com>
2025-06-09 11:04:30 +0100

    solution stable de lundi

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,36 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-export default defineConfig({
-  base: '/',
-  server: {
-    host: true,
-    port: 8080
-  },
-  plugins: [
-    react()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build';
+  
 
-    }
-  }
+  return {
+    base: '/',
+    server: {
+      host: true,
+      port: 8080
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    define: isBuild
+      ? {
+          __DEFINES__: JSON.stringify({}),
+          __HMR_CONFIG_NAME__: JSON.stringify(''),
+          __BASE__: JSON.stringify(''),
+          __SERVER_HOST__: JSON.stringify(''),
+          __HMR_PROTOCOL__: JSON.stringify(''),
+          __HMR_PORT__: JSON.stringify(''),
+          __HMR_HOSTNAME__: JSON.stringify(''),
+          __HMR_BASE__: JSON.stringify(''),
+          __HMR_DIRECT_TARGET__: JSON.stringify(''),
+          __WS_TOKEN__: JSON.stringify(''),
+          __HMR_ENABLE_OVERLAY__: JSON.stringify('')
+        }
+      : {} // 👈 rien en dev pour ne pas casser Vite
+  };
 });
